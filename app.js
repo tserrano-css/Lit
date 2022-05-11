ed4079986d352c788bfcbcaf882161133f5dbb2b
Toni Serrano Suarez <tserrano@cambrasabadell.org>
2022-05-11 18:01:02 +0200

    varios componentes de menu

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 import "@dile/dile-nav/dile-nav.js";
+import "@dile/dile-menu-hamburger/dile-menu-hamburger.js";
+import "@dile/dile-selector/dile-selector.js";
+import "@dile/dile-selector/dile-selector-item.js"; 
+import "@dile/dile-pages/dile-pages.js";
+
