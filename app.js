3ee3eeecdcf53d7292a77891940095096edbd97f
Toni Serrano Suarez <tserrano@cambrasabadell.org>
2022-05-12 16:58:06 +0200

    final clse 2 lit

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ import "@dile/dile-selector/dile-selector.js";
 import "@dile/dile-selector/dile-selector-item.js"; 
 import "@dile/dile-pages/dile-pages.js";
 
+document.getElementById("mainselector").addEventListener("dile-selected-changed", function () {
+  document.querySelector("dile-menu-hamburger").close();
+});
+
--- readme.txt
+++ readme.txt
@@ -2,3 +2,6 @@ Componentes de https://github.com/Polydile/dile-components
 
 Clase del curso:
 https://escuela.it/cursos/curso-lit/clase/usando-componentes-lit
+
+Paleta de colores:
+https://material.io/resources/color/#!/?view.left=0&view.right=0
